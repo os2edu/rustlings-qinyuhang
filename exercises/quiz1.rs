34351a16939f14864b781d9c3ffdd3b968807aab
秦宇航 <qinyuhangxiaoxiang@gmail.com>
2022-11-03 04:20:36 +0000

    quizz1 + primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,11 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number: i32) -> i32 {
+    if number <= 40 { 2 * number } else { number }
+}
 
 // Don't modify this function!
 #[test]
